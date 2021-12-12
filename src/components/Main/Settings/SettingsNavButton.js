4e88f4c0c6704372c59907862b353c267665378a
sankethn <69240053+sankethn@users.noreply.github.com>
2021-12-12 22:02:31 +0530

    Delete src/components directory

--- src/components/Main/Settings/SettingsNavButton.js
+++
@@ -1,9 +0,0 @@
-import styles from "./SettingsNavButton.module.css";
-
-const SettingsNavButton = (props) => {
-  const isActive = props.className ? true : false;
-  console.log(isActive)
-  return <button className={`${styles.btn} ${isActive ? styles.active : ""}`}>{props.children}</button>;
-};
-
-export default SettingsNavButton;
